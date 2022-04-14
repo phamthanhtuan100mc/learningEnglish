265a415dfe61b1d89cc76bfe84b66ea589d5f88a
tuan.pham <tuan.pham@logigear.com>
2022-04-14 23:16:11 +0700

    remove dist folder

--- dist/app/server/main.js
+++
@@ -1,9 +0,0 @@
-function sayMyName(name) {
-    if (name === "Heisenberg") {
-        console.log("You're right 👍");
-    } else {
-        console.log("You're wrong 👎");
-    }
-}
-
-sayMyName("Heisenberg");
